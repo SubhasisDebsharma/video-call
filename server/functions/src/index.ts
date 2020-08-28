94cfacd446e6497178da90d04c7180d7057aa9cd
Subhasis Debsharma(IN4424) <M1044424@mindtree.com>
2020-08-28 14:58:36 +0530

    adding code

---
+++ server/functions/src/index.ts
@@ -0,0 +1,18 @@
+import * as functions from 'firebase-functions';
+import * as firebaseAdmin from 'firebase-admin';
+
+firebaseAdmin.initializeApp();
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
+});
+
+export const makeCall = functions.https.onRequest((request, response) => {
+  response.json({
+    body: request.body,
+  });
+});
