6fba42a21ce5d68ee50c7ae1aa26ca91e41e3552
Subhasis Debsharma(IN4424) <M1044424@mindtree.com>
2020-08-28 19:34:15 +0530

    routing set

--- my-app/src/container/RouterLayout.tsx
+++ my-app/src/container/RouterLayout.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 const Auth = lazy(() => import('./Auth'));
 const Home = lazy(() => import('./Home'));
 
@@ -7,7 +8,7 @@ export default function RouterLayout() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <Switch>
-        <Route path="auth" component={Auth}></Route>
+        <Route path="/auth" component={Auth}></Route>
         <Route path="/" component={Home}></Route>
       </Switch>
     </Suspense>
